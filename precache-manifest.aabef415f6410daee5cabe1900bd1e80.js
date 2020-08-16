942cddea1f0cb3610f1f469ac3530c18dd1df507
hvl579680 <vegy_93@hotmail.com>
2020-08-16 19:32:37 +0200

    Updates

---
+++ precache-manifest.aabef415f6410daee5cabe1900bd1e80.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ba98cd924193dc8144324f5b132c09a5",
+    "url": "/TodoApp/index.html"
+  },
+  {
+    "revision": "07d4790204c4fa2cec22",
+    "url": "/TodoApp/static/css/main.28ff7dce.chunk.css"
+  },
+  {
+    "revision": "a4d3eec0256ebd78e924",
+    "url": "/TodoApp/static/js/2.c12f647a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/TodoApp/static/js/2.c12f647a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "07d4790204c4fa2cec22",
+    "url": "/TodoApp/static/js/main.b31ea095.chunk.js"
+  },
+  {
+    "revision": "43dac9e953fa6ed5b586",
+    "url": "/TodoApp/static/js/runtime-main.a804fe28.js"
+  }
+]);
